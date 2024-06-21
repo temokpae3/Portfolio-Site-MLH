71169e97bcf29bc27ecb14df08546eac59dc742a
TemitopeEmokpae <temokpae@oswego.edu>
2024-06-21 17:31:57 -0400

    Portfolio Tasks complete

--- app/__init__.py
+++ app/__init__.py
@@ -9,3 +9,6 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+
+if __name__ == '__main__':
+    app.run(debug=True)
