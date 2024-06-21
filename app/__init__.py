74a419fa8fa833040d8c7992574e19b87a7e06ab
Emily Zhu <ekzhu@usc.edu>
2024-06-21 14:14:19 -0400

    add hobby page

--- app/__init__.py
+++ app/__init__.py
@@ -9,3 +9,6 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+@app.route('/hobbies')
+def hobbies():
+    return render_template('hobbies.html', title="Hobbies - MLH Fellow", url=os.getenv("URL"))
