da34b0109b886016d3685f33419420a6fbbcafc0
TemitopeEmokpae <temokpae@oswego.edu>
2024-07-12 17:56:34 -0400

    added a bash script, new api endpointa and a new database

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,32 @@
 import os
 from flask import Flask, render_template, request, redirect, url_for
 from dotenv import load_dotenv
+from peewee import *
+import datetime
+from playhouse.shortcuts import model_to_dict
 
 load_dotenv()
 app = Flask(__name__)
+
+mydb = MySQLDatabase(
+    user=os.getenv("MYSQL_USER"),
+    password=os.getenv("MYSQL_PASSWORD"),
+    host=os.getenv("MYSQL_HOST"),
+    database=os.getenv("MYSQL_DATABASE"),
+)
+
+print(mydb)
+
+class TimelinePost(Model):
+    name = CharField()
+    email = CharField()
+    content = TextField()
+    created_at = DateTimeField(default=datetime.datetime.now())
+    
+    class Meta:
+        database = mydb
+mydb.connect()
+mydb.create_tables([TimelinePost])
 
 # Example data for display
 work_experiences = [
@@ -107,4 +130,38 @@ def hobbies_page():
 @app.route('/places')
 def places():
     return render_template('places.html', title="Cool Places I've Visited", url=os.getenv("URL"))
+
+@app.route('/api/timeline_post', methods=['POST'])
+def post_time_line_post():
+    name = request.form['name']
+    email = request.form['email']
+    content = request.form['content']
+    timeline_post = TimelinePost.create(name=name, email=email, content=content)
+    
+    return model_to_dict(timeline_post)
+
+@app.route('/api/timeline_post', methods=['GET'])
+def get_time_line_post():
+    return {
+        'timeline_posts' : [
+            model_to_dict(p) for p in TimelinePost.select().order_by(TimelinePost.created_at.desc())
+        ]
+    }
+
+@app.route('/api/timeline_post/<int:post_id>', methods=['DELETE'])
+def delete_time_line_post(post_id):
+    try:
+        timeline_post = TimelinePost.get_by_id(post_id)
+        timeline_post.delete_instance()
+        return {
+            'message': f'Timeline post with ID {post_id} deleted successfully.'
+        }
+    except DoesNotExist:
+        return {
+            'error': f'Timeline post with ID {post_id} does not exist.'
+        }, 404
+    except Exception as e:
+        return {
+            'error': f'Failed to delete timeline post with ID {post_id}. Error: {str(e)}'
+        }, 500
 
