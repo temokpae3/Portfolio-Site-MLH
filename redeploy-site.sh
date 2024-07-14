9ab03023e83d890191259d24741800a9581804c5
TemitopeEmokpae <temokpae@oswego.edu>
2024-07-14 02:48:19 -0400

    updated my redeploy website

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Killing all existing tmux sessions
-tmux kill-server
-
 # Navigating to the project folder
 cd ~/Portfolio-Site-MLH
 
@@ -14,7 +11,9 @@ source ~/Portfolio-Site-MLH/python3-virtualenv/bin/activate
 pip install -r requirements.txt
 deactivate
 
-# Start a new detached Tmux session
-tmux new-session -d -s flask_app "cd ~/Portfolio-Site-MLH && source ~/Portfolio-Site-MLH/python3-virtualenv/bin/activate && flask run --host=0.0.0.0"
+# Restart the service
+systemctl daemon-reload
+
+systemctl restart myportfolio
 
 echo "Deployment script executed successfully."
